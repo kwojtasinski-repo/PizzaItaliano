cd9f21999a409aea8286c96a95eeeeace72a32f4
KW <k.wojtasinski@wp.pl>
2021-10-30 22:10:48 +0200

    Setup tests in project releases

---
+++ PizzaItaliano.Services.Releases/tests/PizzaItaliano.Services.Releases.Tests.Shared/Helpers/OptionsHelper.cs
@@ -0,0 +1,26 @@
+using Microsoft.Extensions.Configuration;
+using System;
+
+namespace PizzaItaliano.Services.Releases.Tests.Shared.Helpers
+{
+    public class OptionsHelper
+    {
+        public static TSettings GetOptions<TSettings>(string section, string settingsFileName = null) where TSettings : class, new()
+        {
+            settingsFileName ??= "appsettings.tests.json";
+            var configuration = new TSettings();
+
+            GetConfigurationRoot(settingsFileName)
+                .GetSection(section)
+                .Bind(configuration);
+
+            return configuration;
+        }
+
+        private static IConfigurationRoot GetConfigurationRoot(string settingsFileName)
+            => new ConfigurationBuilder()
+                .AddJsonFile(settingsFileName, optional: true)
+                .AddEnvironmentVariables()
+                .Build();
+    }
+}
