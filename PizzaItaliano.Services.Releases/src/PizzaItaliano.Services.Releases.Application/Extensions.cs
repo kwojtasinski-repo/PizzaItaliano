2898ffdf57ffe4bf0c5a1e1a81d4e3b23fbf2f5d
KW <k.wojtasinski@wp.pl>
2021-10-30 22:18:10 +0200

    Added references in Releases Unit Test Project

--- PizzaItaliano.Services.Releases/src/PizzaItaliano.Services.Releases.Application/Extensions.cs
+++ PizzaItaliano.Services.Releases/src/PizzaItaliano.Services.Releases.Application/Extensions.cs
@@ -2,7 +2,9 @@
 using Convey.CQRS.Commands;
 using Convey.CQRS.Events;
 using System;
+using System.Runtime.CompilerServices;
 
+[assembly: InternalsVisibleTo("PizzaItaliano.Services.Releases.Tests.Unit")] // widocznosc internal na poziomie testow (integration)
 namespace PizzaItaliano.Services.Releases.Application
 {
     public static class Extensions
