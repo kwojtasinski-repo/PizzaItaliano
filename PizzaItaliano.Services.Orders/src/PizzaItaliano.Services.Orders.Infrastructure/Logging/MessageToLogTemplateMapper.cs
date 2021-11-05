8459f77fdadffcd7402275cb19de6cec307d61c0
KW <k.wojtasinski@wp.pl>
2021-11-05 23:09:34 +0100

    Added new messages to map on logs

--- PizzaItaliano.Services.Orders/src/PizzaItaliano.Services.Orders.Infrastructure/Logging/MessageToLogTemplateMapper.cs
+++ PizzaItaliano.Services.Orders/src/PizzaItaliano.Services.Orders.Infrastructure/Logging/MessageToLogTemplateMapper.cs
@@ -28,6 +28,38 @@ namespace PizzaItaliano.Services.Orders.Infrastructure.Logging
                 {
                     [typeof(OrderAlreadyExistsException)] = "Order with id: {OrderId} already exists"
                 }
+            },
+            [typeof(AddOrderProduct)] = new HandlerLogTemplate
+            {
+                Before = "Adding an order product with id: {OrderProductId}",
+                After = "Added an order product with id: {OrderProductId}",
+                OnError = new Dictionary<Type, string>
+                {
+                    [typeof(CannotAddOrderProductException)] = "Order product with id: {OrderProductId} cannot be added",
+                    [typeof(OrderNotFoundException)] = "Order with id: {OrderId} not found",
+                    [typeof(ProductNotFoundException)] = "Product with id: {ProductId} not found"
+                }
+            },
+            [typeof(DeleteOrderProduct)] = new HandlerLogTemplate
+            {
+                Before = "Deleting an order product with id: {OrderProductId}",
+                After = "Deleted an order product with id: {OrderProductId}",
+                OnError = new Dictionary<Type, string>
+                {
+                    [typeof(CannotDeleteOrderProductException)] = "Order product with id: {OrderProductId} cannot be deleted",
+                    [typeof(OrderNotFoundException)] = "Order with id: {OrderId} not found",
+                    [typeof(OrderProductNotFoundException)] = "Order product with id: {OrderProductId} not found"
+                }
+            },
+            [typeof(SetOrderStatusReady)] = new HandlerLogTemplate
+            {
+                Before = "Setting status ready for order with id: {OrderId}",
+                After = "Set status ready for order with id: {OrderId}",
+                OnError = new Dictionary<Type, string>
+                {
+                    [typeof(OrderNotFoundException)] = "Order with id: {OrderId} not found",
+                    [typeof(CannotChangeOrderStatusException)] = "Cannot change status for order with id: {OrderId}"
+                }
             }
         };
     }
