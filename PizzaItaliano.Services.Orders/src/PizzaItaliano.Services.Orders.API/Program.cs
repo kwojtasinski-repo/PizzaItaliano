87f4e387fb11bdf88f7c0e4f81858369c9133f41
KW <k.wojtasinski@wp.pl>
2021-11-07 14:13:47 +0100

    Added vault to orders project

--- PizzaItaliano.Services.Orders/src/PizzaItaliano.Services.Orders.API/Program.cs
+++ PizzaItaliano.Services.Orders/src/PizzaItaliano.Services.Orders.API/Program.cs
@@ -16,6 +16,7 @@ using PizzaItaliano.Services.Orders.Infrastructure;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Convey.Logging;
+using Convey.Secrets.Vault;
 
 namespace PizzaItaliano.Services.Orders.API
 {
@@ -26,23 +27,24 @@ namespace PizzaItaliano.Services.Orders.API
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args)
             => WebHost.CreateDefaultBuilder(args)
-                .ConfigureServices(services => services
-                    .AddConvey()
-                    .AddWebApi()
-                    .AddApplication()
-                    .AddInfrastructure()
-                    .Build())
-                .Configure(app => app
-                    .UseInfrastructure()
-                    .UseDispatcherEndpoints(endpoints => endpoints
-                        .Get("", ctx => ctx.Response.WriteAsync(ctx.RequestServices.GetService<AppOptions>().Name))
-                        .Get<GetOrders, IEnumerable<OrderDto>>("orders")
-                        .Get<GetOrder, OrderDto>("orders/{orderId}")
-                        .Post<AddOrder>("orders", afterDispatch: (cmd, ctx) => ctx.Response.Created($"orders/{cmd.OrderId}"))
-                        .Post<AddOrderProduct>("orders/order-product", afterDispatch: (cmd, ctx) => ctx.Response.Ok($"orders/order-product/{cmd.OrderProductId}"))
-                        .Put<SetOrderStatusReady>("orders", afterDispatch: (cmd, ctx) => ctx.Response.Ok($"Set status ready orders/{cmd.OrderId}"))
-                        .Delete<DeleteOrderProduct>("orders/{orderId}/order-product/{orderProductId}/quantity/{quantity:int}", afterDispatch: (cmd, ctx) => ctx.Response.Ok($"Deleted orders/order-product/{cmd.OrderProductId} with quantity {cmd.Quantity}"))
-                    ))
-            .UseLogging();
+                    .ConfigureServices(services => services
+                        .AddConvey()
+                        .AddWebApi()
+                        .AddApplication()
+                        .AddInfrastructure()
+                        .Build())
+                    .Configure(app => app
+                        .UseInfrastructure()
+                        .UseDispatcherEndpoints(endpoints => endpoints
+                            .Get("", ctx => ctx.Response.WriteAsync(ctx.RequestServices.GetService<AppOptions>().Name))
+                            .Get<GetOrders, IEnumerable<OrderDto>>("orders")
+                            .Get<GetOrder, OrderDto>("orders/{orderId}")
+                            .Post<AddOrder>("orders", afterDispatch: (cmd, ctx) => ctx.Response.Created($"orders/{cmd.OrderId}"))
+                            .Post<AddOrderProduct>("orders/order-product", afterDispatch: (cmd, ctx) => ctx.Response.Ok($"orders/order-product/{cmd.OrderProductId}"))
+                            .Put<SetOrderStatusReady>("orders", afterDispatch: (cmd, ctx) => ctx.Response.Ok($"Set status ready orders/{cmd.OrderId}"))
+                            .Delete<DeleteOrderProduct>("orders/{orderId}/order-product/{orderProductId}/quantity/{quantity:int}", afterDispatch: (cmd, ctx) => ctx.Response.Ok($"Deleted orders/order-product/{cmd.OrderProductId} with quantity {cmd.Quantity}"))
+                        ))
+                    .UseLogging()
+                    .UseVault();
     }
 }
