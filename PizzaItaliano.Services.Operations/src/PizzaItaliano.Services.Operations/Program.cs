1916ef02b7fc6cac5cf0fb325faab8b4baf37124
KW <k.wojtasinski@wp.pl>
2022-06-18 21:17:48 +0200

    Issue with UI Operations
    Changed index extension to html
    Added wwwroot folder with contents to csproj

--- PizzaItaliano.Services.Operations/src/PizzaItaliano.Services.Operations/Program.cs
+++ PizzaItaliano.Services.Operations/src/PizzaItaliano.Services.Operations/Program.cs
@@ -1,3 +1,5 @@
+using System;
+using System.IO;
 using System.Threading.Tasks;
 using Convey;
 using Convey.Logging;
@@ -9,6 +11,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Hosting.Internal;
 using Microsoft.Extensions.Logging;
 using PizzaItaliano.Services.Operations.Hubs;
 using PizzaItaliano.Services.Operations.Infrastructure;
