d19e6be5b10f61885346dd4bc005479f20f9b5f3
KW <k.wojtasinski@wp.pl>
2022-07-09 17:33:17 +0200

    Create React App
    Added missing policies

--- pizza-italiano.ui/src/helpers/policiesAuthentication.js
+++ pizza-italiano.ui/src/helpers/policiesAuthentication.js
@@ -1,7 +1,10 @@
 import React from "react";
+import Orders from "../pages/Orders/Orders";
+import { Payments } from "../pages/Payments/Payments";
 import AddProduct from "../pages/Product/AddProduct";
 import EditProduct from "../pages/Product/EditProduct";
 import ViewProduct from "../pages/Product/ViewProduct";
+import { Releases } from "../pages/Releases/Releases";
 
 export function policiesAuthentication(props) {
     let role = '';
@@ -15,6 +18,15 @@ export function policiesAuthentication(props) {
                 role = 'admin';
                 break;
             case ViewProduct : 
+                role = 'admin';
+                break;
+            case Releases : 
+                role = 'admin';
+                break;
+            case Payments :
+                role = 'admin';
+                break;
+            case Orders :
                 role = 'admin';
                 break;
             default:
